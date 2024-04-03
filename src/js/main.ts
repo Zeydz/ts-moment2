4b72746f8cb619d0c5a3f62afa25f3315a32a310
Jocke <joakim03moller@gmail.com>
2024-04-03 11:18:05 +0200

    Lagt till en DOM.TS fil samt börjat på kod

--- src/js/main.ts
+++ src/js/main.ts
@@ -4,3 +4,26 @@ interface Todo {
     completed: boolean,
     priority: number
 }
+/* Klass för todolist */
+export class ToDoList {
+    /* Skapar attribut todos och anger detta som en array som ska lagra objekt */
+    private todos: Todo[];
+    
+    /* Initierar arrayen till en tom array, laddar in from LocalStorage */
+    constructor() {
+        this.todos = [];
+        this.loadFromLocalStorage();
+    }
+
+    /* Localstorage */
+    saveToLocalStorage(): void {
+        localStorage.setItem('todos', JSON.stringify(this.todos));
+    }
+    
+    loadFromLocalStorage(): void {
+        const data = localStorage.getItem('todos');
+        if (data) {
+            this.todos = JSON.parse(data);
+        }
+    }
+}
